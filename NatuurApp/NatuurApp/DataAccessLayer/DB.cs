6d1a5a6ece284bfc4d01b73be0fbb2e410463be9
bertjanstroop <bertjanstroop@hotmail.nl>
2015-03-11 12:14:27 +0100

    eerste commit

---
+++ NatuurApp/NatuurApp/DataAccessLayer/DB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NatuurApp.DataAccessLayer
+{
+    class DB
+    {
+        private DB db = new DB();
+        private DB();
+        public DB GetInstance()
+        {
+            if (db == null)
+            {
+                db = new DB();
+            }
+            return db;
+        }
+    }
+}
