3d4c1eaa9002df5ab584aa947eb31b99642bfa8b
bertjanstroop <bertjanstroop@hotmail.nl>
2015-03-13 11:11:50 +0100

    commit

--- NatuurApp/NatuurApp/DataAccessLayer/DB.cs
+++ NatuurApp/NatuurApp/DataAccessLayer/DB.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using SQLiteClient;
-using System.IO.IsolatedStorage;
+using System.Data.Linq;
+
 namespace NatuurApp.DataAccessLayer
 {
     public class DB
     {
         public DB() { }
-
+        // Create the data context.
+        
         public string GetStringResult(string Query)
         {
             string result = string.Empty;
-            
             return result;
         }
     }
