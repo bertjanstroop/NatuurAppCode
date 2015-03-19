1103311e8e1a3b32c88902418b6ff476ff5c9fe5
Zjoewie <joeyke1@hotmail.com>
2015-03-19 11:12:43 +0100

    add area
    add area

---
+++ NatuurApp/NatuurApp/GUI/AddArea.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using NatuurApp.DataAccessLayer.db;
+
+namespace NatuurApp.GUI
+{
+    public partial class AddArea : PhoneApplicationPage
+    {
+        public AddArea()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddArea_Click(object sender, RoutedEventArgs e)
+        {
+            EmbededDB DB = new EmbededDB();
+            DB.InsertArea(tbAreaName.Text,tbAreaDescription.Text,tbAreaLocation.Text);
+        }
+    }
+}
